521ddb696aec3280fe9a8e3f97a57a46077b93f1
Alex Aalto <alex@aaltointeractive.com>
2018-03-30 12:34:50 -0700

    Cleanup of formatting.

--- www/reply.php
+++ www/reply.php
@@ -1,4 +1,23 @@
 <!doctype html>
+<?php
+function getDateTimeSelector($title,$days,$times) {
+    $html = "<div class=\"row\"><div class=\"col-sm-12\"><h4>$title:</h4></div></div>";
+    $html .= "<div class=\"timeselector panel panel-default\"><div class=\"panel-body\">";
+    foreach($days as $d) {
+        $html .= "<div class=\"row\">";
+        foreach($times as $t) {
+            $fn="$d$t"; // need to fix
+            $html .= "<div class=\"col-sm-2\">";
+            $html .= "<input type=\"checkbox\" name=\"$fn\" id=\"$fn\"/>";
+            $html .= "<label for=\"$fn\">$d $t</label>";
+            $html .= "</div>";
+        }
+        $html .= "</div>";
+    }
+    $html .= "</div></div>";
+    return $html;
+}
+?>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
@@ -10,111 +29,88 @@
   <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
   <script src="js/validate.js"></script>
+  
+  <style>
+  input[type="checkbox"] {
+    margin-right:10px;
+  }
+  </style>
 
     <title>RSVP</title>
   </head>
-  <body class='bg-light'>
-      <div class='container'>
+  <body class="bg-light">
+      <div class="container">
         <main role="main">
         <div class="row">
-            <div class='col-sm-4'>
-                <img height='120' src='img/2018grad.png'/>
+            <div class="col-sm-4">
+                <img height="120" src="img/2018grad.png"/>
             </div>
-            <div class='col-sm-4'>
+            <div class="col-sm-4">
                 
             </div>
-            <div class='col-sm-4'>
-                <img height='60' src='http://www.dut.ac.za/wp-content/themes/stylish-v1.2.4-child/dut-logo.jpg'/>
+            <div class="col-sm-4">
+                <img height="60" src="http://www.dut.ac.za/wp-content/themes/stylish-v1.2.4-child/dut-logo.jpg"/>
             </div>
         </div>
-        <form id='rsvp-form' method='POST' action='save.php'>
+        <form id="rsvp-form" method="POST" action="save.php">
         <h3>Staff Reply Card</h3>
-        <div class="form-group"> <!-- name details -->
-            <div class="row">
-                <label for="title" class="col-sm-2">Title:</label>
-                <select class="col-sm-2" id='frm-title'>
-                    <option></option>
-                    <option>Prof</option>
-                    <option>Dr</option>
-                    <option>Mr</option>
-                    <option>Mrs</option>
-                    <option>Miss</option>
-                    <option>Ms</option>
+        <div class="form-group row"> <!-- name details -->
+            <label for="title" class="col-sm-2 col-form-label">Title:</label>
+            <div class="col-sm-2">
+                <select class="form-control" id="frm-title">
+                    <option value="">Please select a title</option>
+                    <option value="Prof">Prof</option>
+                    <option value="Dr">Dr</option>
+                    <option value="Mr">Mr</option>
+                    <option value="Mrs">Mrs</option>
+                    <option value="Miss">Miss</option>
+                    <option value="Ms">Ms</option>
                 </select>
-                <div class='text-danger' id='err-title'></div>
-            </div>
+                <div class="text-danger" id="err-title"></div>
+			</div>
+    		</div>
             
 <?php
     $pairs = [['Name','Surname'],['Post','Department'],['Telephone','Email']];
     foreach($pairs as $p) {
-        print "\n<div class='row'>";
+        print "\n<div class=\"form-group row\">";
         foreach($p as $f) {
-            print "<label for='$f' class='col-sm-2'>$f:</label>";
-            print "<div class='col-sm-3'>";
-            print "<input type='text' name='$f' id='frm-$f'/>";
-            print "<div id='err-$f'class='text-danger'></div>";
+            print "<label for=\"$f\" class=\"col-sm-2 col-form-label\">$f:</label>";
+            print "<div class=\"col-sm-4\">";
+            print "<input type=\"text\" name=\"$f\" id=\"frm-$f\" class=\"form-control\"/>";
+            print "<div id=\"err-$f\"class=\"text-danger\"></div>";
             print "</div>";
         }
         print "\n</div>";
     }
-?>
-        </div>
-        <div class="form-group">
-            <h4>Midlands:</h4>
-<?php
-    $days = ['25 Apr','26 Apr'];
-    $times = ['10:00','13:00'];
-    foreach($days as $d) {
-        print "\n<div class='row'>";
-        print "\n<div class='col-sm-2'>$d</div>";
-        foreach($times as $t) {
-            $fn="$d$t"; // need to fix
-            print "\n<label for='$fn' class='col-sm-1'>$t</label>";
-            print "\n<input type='checkbox' name='$fn' id='$fn' class='col-sm-1'/>";
-        }
-        print "</div>";
-    }
-?>
-
-            <h4>Durban:</h4>
-<?php
-    $days = ['07 May','08 May','09 May','10 May','14 May'];
-    $times = ['09:00','14:00','18:00'];
-    foreach($days as $d) {
-        print "\n<div class='row'>";
-        print "\n<div class='col-sm-2'>$d</div>";
-        foreach($times as $t) {
-            $fn="$d$t"; // need to fix
-            print "\n<label for='$fn' class='col-sm-1'>$t</label>";
-            print "\n<input type='checkbox' name='$fn' id='$fn' class='col-sm-1'/>";
-        }
-        print "</div>";
-    }
-?>
-            
-        </div>
-        <div class="form-group">
-<div class='row'>
-    <label for='procession' class='col-sm-5'>
-        I wish to participate in the academic procession:
-    </label>
-    <input type='checkbox' name='procession'id='procession' class='col-sm-1'/>
-</div>            
-<div class='row'>
-    <label for='spouse' class='col-sm-5'>
-        I will be accompanied by my spouse/partner:
-    </label>
-    <input type='checkbox' name='spouse'id='spouse' class='col-sm-1'/>
-</div>            
-<div class='row'>
-    <label for='mailto' class='col-sm-5'>
-        Address to which admission cards should be sent:
-    </label>
-    <textarea class='col-sm-5' id='frm-Address' rows='4' name='Address'></textarea>
-    <<div class='text-danger' id='err-Address'></div>
-</div>            
-        </div>
-        <input type='submit'/>
+    
+    print getDateTimeSelector('Midlands',['25 Apr','26 Apr'],['10:00','13:00']);
+    print getDateTimeSelector('Durban',['07 May','08 May','09 May','10 May','14 May'],['09:00','14:00','18:00']);
+    ?>            
+        <div class="row">
+    			<div class="col-sm-12">
+    				<input type="checkbox" name="procession"id="procession"/>
+    				<label for="procession">I wish to participate in the academic procession:</label>
+    			</div>
+		</div>            
+		<div class="form-group row">
+			<div class="col-sm-12">
+				<input type="checkbox" name="spouse" id="spouse"/>
+    				<label for="spouse">I will be accompanied by my spouse/partner:</label>    
+    			</div>
+		</div>            
+		<div class="form-group row">
+			<div class="col-sm-12">
+	    			<label for="mailto">Address to which admission cards should be sent:</label>
+				<textarea class="form-control" id="frm-Address" rows="4" name="Address"></textarea>
+				<div class="text-danger" id="err-Address"></div>
+			</div>
+		</div>
+        <div class="form-group row">
+        		<div class="col-sm-12" style="text-align:right;">
+				<input type="submit" class="btn btn-primary" />
+			</div>
+		</div>
         </form>
         </main>
     </div>
