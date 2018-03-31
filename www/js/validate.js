4cb5fbdd6ff404ea77a120a793ac30e93214ab9b
Alex Aalto <alex@aaltointeractive.com>
2018-03-31 16:29:44 -0700

    Cleaned up HTML
    - Cleaned HTML in reply.php to properly leverage Bootstrap
    - Rewrote validation to be more generic
    - Added new CSS file

--- www/js/validate.js
+++ www/js/validate.js
@@ -1,34 +1,29 @@
-//
-// 
-
 $(document).ready(function() {
     $('#rsvp-form').submit(validateFormData);
 });
 
 function validateFormData(e) {
-    //alert("validating...");
-    // clear all previous error messages
-    $('div.text-danger').text("");
-    var errcount = 0;
-    
-    // check title select
-    var ndx = $('#frm-title')[0].selectedIndex ;
-    if(ndx == 0) {
-        $('#err-title').text("Indicate your title");
-        errcount++;
+	// Boolean for is good:
+	var isgood = true;
+	// Clear previous errors:
+	$('.required .text-danger').remove();
+	$('.has-error').removeClass('has-error');
+	// Check the required fields:
+    var flds = $('.required input,.required select,.required textarea');
+    for(var i=0;i<flds.length;i++) {    	
+    		var v = $(flds[i]).val();
+    		var n = $(flds[i]).attr('name');
+    		// If there is a value, trim it radically:
+    		if(v!=null) v = v.replace(/\s/g,'');
+    		// Check to see if there is anything left:
+    		if(v==null || v.length==0) {
+    			// Append an error message to the parent element:
+    			$(flds[i]).parent().append('<div class="text-danger">'+n+' must not be blank.</div>');
+    			// Add the bootstrap class to light up input fields:
+    			$(flds[i]).parent().addClass('has-error');
+    			// Set our return var to false:
+    			isgood = false;
+    		}
     }
-    
-    // check input fields
-    var fields = ['Name','Surname','Post','Department','Telephone','Email','Address'];
-    for(var i in fields) {
-        var fname = fields[i];
-        var val = $('#frm-'+fname)[0].value;
-        if(val == null || val == "") {
-            $('#err-'+fname).text(fname+" must not be blank");
-            errcount++;
-        } else {
-            //$('#err-'+fname).text(fname+" is OK");
-        }
-    }
-    return (errcount == 0);
+    return isgood;
 }
--- www/reply.php
+++ www/reply.php
@@ -1,20 +1,22 @@
 <!doctype html>
 <?php
-function getDateTimeSelector($title,$days,$times) {
-    $html = "<div class=\"row\"><div class=\"col-sm-12\"><h4>$title:</h4></div></div>";
+function getDateTimeSelector($title,$clazz,$days,$times) {
+    $html = "<div class=\"row\"><div class=\"col-sm-12\"><h4 class=\"required\">$title:</h4></div></div>";
     $html .= "<div class=\"timeselector panel panel-default\"><div class=\"panel-body\">";
     foreach($days as $d) {
-        $html .= "<div class=\"row\">";
+        $html .= '<div class="row">';
+        $ctr = 0;
         foreach($times as $t) {
+            $id = $clazz.'-'.++$ctr;
             $fn="$d$t"; // need to fix
-            $html .= "<div class=\"col-sm-2\">";
-            $html .= "<input type=\"checkbox\" name=\"$fn\" id=\"$fn\"/>";
-            $html .= "<label for=\"$fn\">$d $t</label>";
-            $html .= "</div>";
+            $html .= '<div class="col-sm-2">';
+            $html .= "<input type=\"checkbox\" name=\"$fn\" id=\"$id\" class=\"$clazz\"/>";
+            $html .= "<label for=\"$id\">$d $t</label>";
+            $html .= '</div>';
         }
-        $html .= "</div>";
+        $html .= '</div>';
     }
-    $html .= "</div></div>";
+    $html .= '</div></div>';
     return $html;
 }
 ?>
@@ -25,18 +27,13 @@ function getDateTimeSelector($title,$days,$times) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-  <script src="js/validate.js"></script>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link href="css/styles.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="js/validate.js"></script>
   
-  <style>
-  input[type="checkbox"] {
-    margin-right:10px;
-  }
-  </style>
-
-    <title>RSVP</title>
+	<title>RSVP</title>
   </head>
   <body class="bg-light">
       <div class="container">
@@ -54,10 +51,10 @@ function getDateTimeSelector($title,$days,$times) {
         </div>
         <form id="rsvp-form" method="POST" action="save.php">
         <h3>Staff Reply Card</h3>
-        <div class="form-group row"> <!-- name details -->
+        <div class="form-group row required"> <!-- name details -->
             <label for="title" class="col-sm-2 col-form-label">Title:</label>
             <div class="col-sm-2">
-                <select class="form-control" id="frm-title">
+                <select class="form-control" name="Title" id="frm-title">
                     <option value="">Please select a title</option>
                     <option value="Prof">Prof</option>
                     <option value="Dr">Dr</option>
@@ -66,40 +63,37 @@ function getDateTimeSelector($title,$days,$times) {
                     <option value="Miss">Miss</option>
                     <option value="Ms">Ms</option>
                 </select>
-                <div class="text-danger" id="err-title"></div>
 			</div>
     		</div>
             
 <?php
     $pairs = [['Name','Surname'],['Post','Department'],['Telephone','Email']];
     foreach($pairs as $p) {
-        print "\n<div class=\"form-group row\">";
+        print "\n<div class=\"form-group row required\">";
         foreach($p as $f) {
             print "<label for=\"$f\" class=\"col-sm-2 col-form-label\">$f:</label>";
             print "<div class=\"col-sm-4\">";
             print "<input type=\"text\" name=\"$f\" id=\"frm-$f\" class=\"form-control\"/>";
-            print "<div id=\"err-$f\"class=\"text-danger\"></div>";
             print "</div>";
         }
         print "\n</div>";
     }
-    
-    print getDateTimeSelector('Midlands',['25 Apr','26 Apr'],['10:00','13:00']);
-    print getDateTimeSelector('Durban',['07 May','08 May','09 May','10 May','14 May'],['09:00','14:00','18:00']);
+    print getDateTimeSelector('Midlands','datetime-midlands',['25 Apr','26 Apr'],['10:00','13:00']);
+    print getDateTimeSelector('Durban','datetime-durban',['07 May','08 May','09 May','10 May','14 May'],['09:00','14:00','18:00']);
     ?>            
         <div class="row">
     			<div class="col-sm-12">
     				<input type="checkbox" name="procession"id="procession"/>
-    				<label for="procession">I wish to participate in the academic procession:</label>
+    				<label for="procession">I wish to participate in the academic procession</label>
     			</div>
 		</div>            
 		<div class="form-group row">
 			<div class="col-sm-12">
 				<input type="checkbox" name="spouse" id="spouse"/>
-    				<label for="spouse">I will be accompanied by my spouse/partner:</label>    
+    				<label for="spouse">I will be accompanied by my spouse/partner</label>    
     			</div>
 		</div>            
-		<div class="form-group row">
+		<div class="form-group row required">
 			<div class="col-sm-12">
 	    			<label for="mailto">Address to which admission cards should be sent:</label>
 				<textarea class="form-control" id="frm-Address" rows="4" name="Address"></textarea>
